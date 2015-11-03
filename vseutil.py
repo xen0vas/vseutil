0757c3dff3ba6c1e9d0d8c0027cf87093474fca4
xvass <xenofonv@gmail.com>
2015-11-03 11:40:45 +0200

    new working version v12

--- vseutil.py
+++ vseutil.py
@@ -14,7 +14,6 @@ import sys
 import platform as winos
 import wmi
 from netaddr import *
-import pprint
 from colorama import *
 import shutil
 import win32wnet
@@ -25,6 +24,7 @@ import win32api
 import win32con
 import win32service
 import win32serviceutil
+from datetime import date
 import win32event
 
 RUNNING = win32service.SERVICE_RUNNING
@@ -36,7 +36,35 @@ init()
 
 def main():
 	
-	parser = optparse.OptionParser('\n\nAuthor: Xenofon Vassilakopoulos (@xvass) \n\n[-] Usage: \n\n 1) vse88x.exe  -s <from_target> -t <to_target> -u <domain\username> -p <password> -r "<value>"\n\n 2) vse88x.exe -c target_ip -u <domain\username> -p <password> --sf <src_file> --df <share_folder> -r "<value>" \n\n 3) vse88x.exe -s <from_target> -t <to_target> -u <domain\username> -p <password> --sf <src_file> --df <share_folder> -r "<value>"\n\n 4) vse88x.exe -c <ip/cidr> -u <domain\username> -p <password> --sf <src_file> --df <share_folder> -r "<value>" \n\n 5) vse88x.exe -c <ip/cidr> -u <domain\username> -p <password> -r "<value>"\n\n 6) vse88x.exe -c <ip> -u <domain\username> -p <password> -r <"value">\n\n- Registry Values:\n\n- DATVersion\n- Version\n- DatInstallDate\n- HotFixVersions\n- Uninstall Command\n- EngineVersion\n- DatDate\n- EngineInstallDate\n- Install Path\n- Installs CMA\n- Plugin Flag\n- Plugin Path\n- Software ID\n- McTrayAboutBoxDisplay\n- Enforce Flag\n- CLSID\n- Language\n- Product Name\n')
+	parser = optparse.OptionParser('\n\nAuthor: Xenofon Vassilakopoulos (@xvass) \n\n\
+	[-] Usage: \n\n 1) vse88x.exe  -s <from_target> -t <to_target> -u <domain\username> -p <password> -r "<value>"\n\n \
+	2) vse88x.exe -c target_ip -u <domain\username> -p <password> --sf <src_file> --df <share_folder> -r "<value>" \n\n \
+	3) vse88x.exe -s <from_target> -t <to_target> -u <domain\username> -p <password> --sf <src_file> --df <share_folder> -r "<value>"\n\n \
+	4) vse88x.exe -c <ip/cidr> -u <domain\username> -p <password> --sf <src_file> --df <share_folder> -r "<value>" \n\n \
+	5) vse88x.exe -c <ip/cidr> -u <domain\username> -p <password> -r "<value>"\n\n \
+	6) vse88x.exe -c <ip> -u <domain\username> -p <password> -r <"value">\n\n- \
+	Registry Values:\n\n- \
+	DATVersion\n- \
+	Version\n- \
+	DatInstallDate\n- \
+	HotFixVersions\n- \
+	Uninstall Command\n- \
+	EngineVersion\n- \
+	DatDate\n- \
+	EngineInstallDate\n- \
+	Install Path\n- \
+	Installs CMA\n- \
+	Plugin Flag\n- \
+	Plugin Path\n- \
+	Software \
+	ID\n- \
+	McTrayAboutBoxDisplay\n- \
+	Enforce Flag\n- \
+	CLSID\n- \
+	Language\n- \
+	Product Name\n')
+	
+	
 	parser.add_option('-r', dest = 'regname', type ='string',help = 'registry key')
 	parser.add_option('--out', dest = 'output_file', type='string', help = 'output to file')
 	parser.add_option('-u', dest = 'uname', type='string',help='user name with domain or workgroup')
@@ -46,7 +74,9 @@ def main():
 	parser.add_option('-c', dest = 'tocidrhost', type='string',help = 'cidr')
 	parser.add_option('--sf', dest = 'sourcefile', type='string',help = 'sourcedir/sourcefile')
 	parser.add_option('--df', dest = 'destinationfile', type='string',help = 'destdir/destfile')
-	parser.add_option('-d', dest = 'downgrade', type='string',help='downgrade DAT version')
+	parser.add_option('--hlp', dest = 'help', type='string',help='show options and registry keys')
+	parser.add_option('-l', dest = 'localhost', type='string',help='run into local host')
+	parser.add_option('-d', dest = 'down', type='string',help='downgrade DAT')
 	
 	(options,args) = parser.parse_args()
 	
@@ -59,7 +89,9 @@ def main():
 	sourcefile = options.sourcefile
 	destfile = options.destinationfile
 	outfile = options.output_file
-	down = options.downgrade
+	help = options.help
+	local = options.localhost
+	down = options.down
 	
 	
 	if (value == None and tgtuser == None and tgtpass == None and from_host == None and to_host == None):
@@ -67,7 +99,7 @@ def main():
 			print parser.usage
 			sys.exit()
 	
-	connectwmi(from_host,to_host,tgtuser,tgtpass,value,cidr_hosts,sourcefile,destfile,outfile)
+	connectwmi(from_host,to_host,tgtuser,tgtpass,value,cidr_hosts,sourcefile,destfile,outfile,down)
 		
 def log_to_file(message,outfile): 
 	fd=open(outfile,"ab" )
@@ -103,7 +135,10 @@ def wnet_connect(host, username, password):
 			if err[0] == 1219:
 				win32wnet.WNetCancelConnection2(unc, 0, 0)
 				return wnet_connect(host, username, password)
-			raise err		
+			raise err
+def DATdate():
+	today = date.today()
+	return str(today).replace("-","")
 
 def copy_file(DAT2val,ip,user,password,sourcefile,destfile,outfile):
 	semaphore = threading.BoundedSemaphore()
@@ -202,7 +237,7 @@ def registry_values(username,upass,value,sourcefile,destfile,c,outfile):
 			
 	return (val,arch,DAT2val,valnum,DAT)
 
-# This function converts and modifies strings 
+# This function converts and modifies strings carrying DAT lexical format 
 def string_convert(val,sourcefile):
 	splitval = val 
 	valsplit = splitval.split('.')
@@ -215,7 +250,7 @@ def string_convert(val,sourcefile):
 	valnum = int(valsplit[0])
 	return (DAT2val,valnum,DAT)
 
-# This function updates DAT files inside the Engine file. It requires McShield and McAfeeFramework
+# This function updates DAT files inside the Engine file. It requires McShield and McAfeeFramework to stop and after the files copied then start.
 def update_vse(DAT,ip,DAT2val,valnum,username,upass,sourcefile,destfile,outfile):
 	semaphore = threading.BoundedSemaphore()
 	semaphore.acquire()
@@ -270,38 +305,40 @@ def update_vse(DAT,ip,DAT2val,valnum,username,upass,sourcefile,destfile,outfile)
 		deletefiles(ip,destfile,DAT,outfile)		
 		svcStart( "McShield",arg, unicode(ip))	
 		svcStart( "McAfeeFramework",arg, unicode(ip))
-		state_value="passed"
-	
+		state_value="passed"	
 	status1 = svcStatus( "McShield", unicode(ip))
 	status2 = svcStatus( "McAfeeFramework", unicode(ip))
-	semaphore.release()
-	
+	semaphore.release()	
 	return (status1,status2,state_value)
 
 #This function updates registry values only when Engine folder has been updated 
 def update_registry(status1,status2,state_value,arch,outfile,c,value,DAT2val):
 	semaphore = threading.BoundedSemaphore()
 	semaphore.acquire()
+	today = DATdate()
 	if status1 != STOPPED and status2 != STOPPED and state_value == "passed":									
 		if(arch == 'x86'):
 			print "[*] updating registry.."
 			if (outfile != None):
 				log_to_file("[*] updating registry..",outfile)
 			result, = c.SetStringValue(hDefKey=win32con.HKEY_LOCAL_MACHINE,sSubKeyName=r"SOFTWARE\Network Associates\ePolicy Orchestrator\Application Plugins\VIRUSCAN8800",sValueName=value,sValue=str(DAT2val) + '.0000')
+			dateres, = c.SetStringValue(hDefKey=win32con.HKEY_LOCAL_MACHINE,sSubKeyName=r"SOFTWARE\Network Associates\ePolicy Orchestrator\Application Plugins\VIRUSCAN8800",sValueName='DatDate',sValue=today)
 			res,val = c.GetStringValue(hDefKey=win32con.HKEY_LOCAL_MACHINE,sSubKeyName="SOFTWARE\Network Associates\ePolicy Orchestrator\Application Plugins\VIRUSCAN8800",sValueName=value)
-			print "[*] registry updated succesfully"
+			print "[*] registry updated successfully"
 			if (outfile != None):
-				log_to_file("[*] registry updated succesfully",outfile)
+				log_to_file("[*] registry updated successfully",outfile)
 		else:
 			print "[*] updating registry.."
 			if (outfile != None):
 				log_to_file("[*] updating registry..",outfile)
 			result, = c.SetStringValue(hDefKey=win32con.HKEY_LOCAL_MACHINE,sSubKeyName=r"SOFTWARE\Wow6432Node\Network Associates\ePolicy Orchestrator\Application Plugins\VIRUSCAN8800",sValueName=value,sValue=str(DAT2val) + '.0000')
+			dateres, = c.SetStringValue(hDefKey=win32con.HKEY_LOCAL_MACHINE,sSubKeyName=r"SOFTWARE\Wow6432Node\Network Associates\ePolicy Orchestrator\Application Plugins\VIRUSCAN8800",sValueName='DatDate',sValue=today)
 			res,val = c.GetStringValue(hDefKey=win32con.HKEY_LOCAL_MACHINE,sSubKeyName="SOFTWARE\Wow6432Node\Network Associates\ePolicy Orchestrator\Application Plugins\VIRUSCAN8800",sValueName=value)
-			print "[*] registry updated succesfully"
+			print "[*] registry updated successfully"
 			if (outfile != None):
-				log_to_file("[*] registry updated succesfully",outfile)
-		print  Fore.WHITE + "[*] new current %s is" % (value) + Fore.YELLOW + " %s \n\n" % (val)
+				log_to_file("[*] registry updated successfully",outfile)
+		print  Fore.WHITE + "[*] new current %s is" % (value) + Fore.YELLOW + " %s " % (val)
+		print "[*] Exiting...\n"
 		if (outfile != None):
 			log_to_file("[*] new current %s " % (value) + "is %s \n\n" % (val),outfile)
 	elif state_value == "not_passed":
@@ -309,9 +346,28 @@ def update_registry(status1,status2,state_value,arch,outfile,c,value,DAT2val):
 		if (outfile != None):
 			log_to_file("[*] Registry cannot be updated. Please try again..\n",outfile)
 	semaphore.release()
-	
-def connectwmi(fromh,toh,username,upass,value,cidr_hosts,sourcefile,destfile,outfile):
-				
+
+def changeDATversion(DAT,ip,DAT2val,valnum,username,upass,sourcefile,destfile,outfile,arch,c,value,val,level):
+	semaphore = threading.BoundedSemaphore()
+	semaphore.acquire()
+	if DAT2val != valnum and level=="down":
+		status1,status2,state_value = update_vse(DAT,ip,DAT2val,valnum,username,upass,sourcefile,destfile,outfile)
+		update_registry(status1,status2,state_value,arch,outfile,c,value,DAT2val)
+	elif DAT2val != valnum and level == None and DAT2val > valnum:
+		status1,status2,state_value = update_vse(DAT,ip,DAT2val,valnum,username,upass,sourcefile,destfile,outfile)
+		update_registry(status1,status2,state_value,arch,outfile,c,value,DAT2val)
+	elif DAT2val == valnum or (DAT2val < valnum and level==None):
+		print Fore.WHITE + "[*] current %s " % (value) + "is " + Fore.YELLOW +  "%s " % (val)
+		if (DAT2val < valnum):
+			print "[*] You are trying to install a lower DAT version..Use option '-d down' to downgrade.."
+		print "[*] Exiting..."
+		if (outfile != None):
+			log_to_file("[*] current %s " % (value) + "is %s" % (val),outfile)
+			log_to_file("[*] Exiting...",outfile)
+	semaphore.release()
+	return
+
+def connectwmi(fromh,toh,username,upass,value,cidr_hosts,sourcefile,destfile,outfile,level):				
 	if (cidr_hosts != None):
 		iprange = IPNetwork(cidr_hosts)
 	else:
@@ -326,26 +382,18 @@ def connectwmi(fromh,toh,username,upass,value,cidr_hosts,sourcefile,destfile,out
 			c = wmiconnect(ip,username,upass,outfile)
 			if (c != "not_connected"):
 				val,arch,DAT2val,valnum,DAT = registry_values(username,upass,value,sourcefile,destfile,c,outfile)
-				#user,domain = credentials(username,upass)
-				if (sourcefile != None or destfile != None):
-					if DAT2val > valnum and DAT2val != valnum:
-						try:				
-							status1,status2,state_value = update_vse(DAT,ip,DAT2val,valnum,username,upass,sourcefile,destfile,outfile)
-						except:
-							print "[*] Cannot access services..check permissions"
-							continue
-						update_registry(status1,status2,state_value,arch,outfile,c,value,DAT2val)			
-						
-					elif DAT2val <= valnum:
-						print Fore.WHITE + "[*] current %s " % (value) + "is " + Fore.YELLOW +  "%s " % (val)
-						if (outfile != None):
-							log_to_file("[*] current %s " % (value) + "is %s" % (val),outfile)
-											
+				if (sourcefile != None or destfile != None):				
+					try:						
+						changeDATversion(DAT,ip,DAT2val,valnum,username,upass,sourcefile,destfile,outfile,arch,c,value,val,level)										
+					except:
+						print "[*] Cannot access services..check permissions"
+						print "[*] Exiting..."
+						continue
 			else:
 				print "[*] Not connected to host with IP address %s" % ip + " Probably the host is down or user is logged off"
 				if (outfile != None):
 					log_to_file("[*] Not connected to IP address %s" % ip + " Probably the host is down or user is logged off",outfile)
-		except win32service.error, (hr, fn, msg):
+		except win32service.error, (msg):
 			print "[*] Error starting service: %s" % msg
 			if (outfile != None):
 				log_to_file("[*] Error starting service: %s" % msg,outfile)
